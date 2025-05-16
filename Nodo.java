db916c356392269842cedeb5f80779e3e5fa51f1
valeriabet <valeriabetancourt013@gmail.com>
2025-05-15 19:31:46 -0500

    Se crea la clase nodo
    Se crea completa la clase Nodo para el taller

---
+++ Nodo.java
@@ -0,0 +1,11 @@
+public class Nodo {
+    int valor;
+    Nodo izquierdo;
+    Nodo derecho;
+
+    public Nodo (int valor){
+        this.valor = valor;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+}
